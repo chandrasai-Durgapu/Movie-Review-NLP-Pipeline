11293c65f078a3af503e403c5d98528cf1691ea0
chandra sai durgapu <chandrasekharcse522@gmail.com>
2025-10-02 19:13:58 +0530

    Update register_model.py

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -22,8 +22,8 @@ warnings.filterwarnings("ignore")
 # os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 # dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+# repo_owner = "your-repo-"
+# repo_name = "Your-Project"
 # # Set up MLflow tracking URI
 # mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
